6a515a915f5381b92e0067217bc40d9c55cf64ac
Brian-3112 <pepito123@gmail.com>
2023-06-22 20:15:22 -0500

    hola

---
+++ js/funcionalidadesProveedor/listarProveedor.js
@@ -0,0 +1,11 @@
+import {listarProveedor} from '../funcionalidadesProveedor/UIProveedor.js';
+
+
+(() => {
+    document.addEventListener('DOMContentLoaded', () => {
+        listarProveedor();
+        // imprimirPaginador();
+        // buscarRegistros(1); 
+        // peticionesBackend.findAll();
+    });
+})();
