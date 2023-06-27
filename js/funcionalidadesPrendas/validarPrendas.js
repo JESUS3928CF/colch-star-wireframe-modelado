1aa71c81006e340fae4f9e2f46080a92845d2613
Ts2533 <tomasvanegas2533@gmail.com>
2023-06-27 08:03:21 -0500

    Validacion de prendas listo

--- js/funcionalidadesPrendas/validarPrendas.js
+++ js/funcionalidadesPrendas/validarPrendas.js
@@ -2,6 +2,13 @@
 
     const formulario= document.querySelector('#formularioAgregarPrenda');
     const formularioEditar=document.querySelector('#formularioEditarPrendas');
+    const formulario2=document.querySelector('#Tallaje')
+    const formulario3=document.querySelector('#Colore')
+    const formulario4=document.querySelector('#Sexo')
+
+
+
+
 
     const submitEditar=document.querySelector('#Editar')
     const submit = document.querySelector('#buttonSubmit');
@@ -36,6 +43,9 @@ function recetearFormulario(e) {
     e.preventDefault();
     formulario.reset();
     formularioEditar.reset();
+    formulario2.reset();
+    formulario3.reset();
+    formulario4.reset();
 }
 
 
@@ -49,6 +59,37 @@ function variable(){
     const precio = document.querySelector('#precioEditar')
     const talla = document.querySelector('#tallaEditar')
     const tela =document.querySelector('#telaEditar')
+    const Stelas = document.querySelector('#SelectT');
+    const seleT = Stelas.options[Stelas.selectedIndex].value;
+    const TallaOcho = document.forms['Tallaje']['TallaOcho'].checked
+    const TallaDiesiseis = document.forms['Tallaje']['TallaDiesiseis'].checked
+    const TallaS = document.forms['Tallaje']['TallaS'].checked
+    const TallaM = document.forms['Tallaje']['TallaM'].checked
+    const TallaL = document.forms['Tallaje']['TallaL'].checked
+    const TallaXL = document.forms['Tallaje']['TallaXL'].checked
+    
+    const Negro = document.forms['Colore']['Negro'].checked
+    const Rojo = document.forms['Colore']['Rojo'].checked
+    const Azul = document.forms['Colore']['Azul'].checked
+    const Blanco = document.forms['Colore']['Blanco'].checked
+    const Rosado = document.forms['Colore']['Rosado'].checked
+    const Morado = document.forms['Colore']['Morado'].checked
+    const Azulclaro = document.forms['Colore']['AzulClaro'].checked
+    const Verde = document.forms['Colore']['Verde'].checked
+
+    const masculino = document.forms['Sexo']['Masculino'].checked
+    const femenino = document.forms['Sexo']['Femenino'].checked
+
+
+
+    const  negroEditar = document.forms['Editar']['negroEditar'].checked
+    const  rojoEditar= document.forms['Editar']['rojoEditar'].checked
+    const  azulEditar= document.forms['Editar']['azulEditar'].checked
+    const  verdeEditar= document.forms['Editar']['verdeEditar'].checked
+    const  grisEditar= document.forms['Editar']['grisEditar'].checked
+
+
+
     isvalidar = true;
     const number = /^\D*$/;
     const text = /^[^a-zA-Z]*$/;
@@ -67,7 +108,29 @@ function variable(){
         Productos: producto,
         Precio:precio,
         Talla:talla,
-        Tela:tela
+        Tela:tela,
+        telas:seleT,
+        TallaO:TallaOcho,
+        TallaD:TallaDiesiseis,
+        TallaM:TallaM,
+        TallaS:TallaS,
+        TallaL:TallaL,
+        TallaXL:TallaXL,
+        ColorNegro:Negro,
+        ColorRojo:Rojo,
+        ColorAzul:Azul,
+        ColorBlanco:Blanco,
+        ColorRosado:Rosado,
+        ColorMorado:Morado,
+        ColorAzulClaro:Azulclaro,
+        ColorVerde:Verde,
+        M: masculino,
+        F: femenino,
+        negroEditar,
+        rojoEditar,      
+        azulEditar,      
+        verdeEditar,     
+        grisEditar
 
     }
 }
@@ -79,7 +142,7 @@ function ValidarPrenda (){
     const Datos = variable()
 
     //Validar los campos del formulario
-    if(Datos.prendas.value=="" && Datos.proveedor.value=="" && Datos.cantidaprenda.value=="" ){
+    if(Datos.prendas.value=="" && Datos.proveedor.value=="" && Datos.cantidaprenda.value=="" &&  Datos.ColorAzul==false && Datos.ColorAzulClaro==false && Datos.ColorBlanco==false && Datos.ColorMorado==false && Datos.ColorNegro==false && Datos.ColorRojo==false && Datos.ColorRosado==false && Datos.ColorVerde==false  && Datos.TallaO==false && Datos.TallaD==false && Datos.TallaS==false && Datos.TallaM==false && Datos.TallaL==false && Datos.TallaXL==false && Datos.M==false && Datos.F==false){
         Swal.fire({
             icon: 'error',
             title: 'Error',
@@ -178,6 +241,48 @@ function ValidarPrenda (){
         })
         Datos.Validar=false
 
+    }else if(Datos.telas==""){
+        Swal.fire({
+            icon:'error',
+            title: 'Error',
+            text: 'Seleccione una tela'
+        })
+        Datos.Validar=false
+
+    }else if(Datos.TallaO==false && Datos.TallaD==false && Datos.TallaS==false && Datos.TallaM==false && Datos.TallaL==false && Datos.TallaXL==false){
+        Swal.fire({
+            icon:'error',
+            title: 'Error',
+            text: 'Seleccione una talla'
+        })
+        Datos.Validar=false
+
+
+    }else if (Datos.ColorAzul==false && Datos.ColorAzulClaro==false && Datos.ColorBlanco==false && Datos.ColorMorado==false && Datos.ColorNegro==false && Datos.ColorRojo==false && Datos.ColorRosado==false && Datos.ColorVerde==false){
+        Swal.fire({
+            icon:'error',
+            title: 'Error',
+            text: 'Seleccione un color'
+        })
+        Datos.Validar=false
+
+
+    }else if (Datos.M==false && Datos.F==false){
+        Swal.fire({
+            icon:'error',
+            title: 'Error',
+            text: 'Seleccione el genero'
+        })
+        Datos.Validar=false
+
+    }else if (Datos.M==true && Datos.F==true){
+        Swal.fire({
+            icon:'error',
+            title: 'Error',
+            text: 'Solo selecciona un  genero'
+        })
+        Datos.Validar=false
+
     }
 
     if (Datos.Validar){
@@ -194,7 +299,7 @@ function ValidarPrenda (){
 function Editar(){
 const Datos=variable();
 
-if(Datos.Productos.value=="" && Datos.cantidaprenda.value=="" && Datos.Precio.value=="" && Datos.Talla.value=="" && Datos.Tela.value==""){
+if(Datos.Productos.value=="" && Datos.cantidaprenda.value=="" && Datos.Precio.value=="" && Datos.Talla.value=="" && Datos.Tela.value=="" &&  Datos.negroEditar==false && Datos.rojoEditar==false && Datos.azulEditar==false && Datos.verdeEditar==false && Datos.grisEditar==false){
         Swal.fire({
             icon: 'error',
             title: 'Error',
@@ -346,6 +451,13 @@ if(Datos.Productos.value=="" && Datos.cantidaprenda.value=="" && Datos.Precio.va
         icon:'error',
         title: 'Error',
         text: 'El campo Tela no puede ser espacio'
+    })
+    Datos.Validar=false
+}else if ( Datos.negroEditar==false && Datos.rojoEditar==false && Datos.azulEditar==false && Datos.verdeEditar==false && Datos.grisEditar==false){
+    Swal.fire({
+        icon:'error',
+        title: 'Error',
+        text: 'Selecciona un color'
     })
     Datos.Validar=false
 }
