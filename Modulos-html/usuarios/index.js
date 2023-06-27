96123cf672e55beeec3adce57e714f8a670e31f1
Ts2533 <tomasvanegas2533@gmail.com>
2023-06-26 23:54:10 -0500

    Modal de login listo

--- Modulos-html/usuarios/index.js
+++ Modulos-html/usuarios/index.js
@@ -1,3 +1,4 @@
+const formulario = document.querySelector('#login');
 const loginsec=document.querySelector('.login-section')
 const loginlink=document.querySelector('.login-link')
 const registerlink=document.querySelector('.register-link')
@@ -11,10 +12,101 @@ loginlink.addEventListener('click',()=>{
     loginsec.classList.remove('active')
 })
 
-submit.addEventListener("click", ingresar);
+window.addEventListener('load', () => {
+    submit.addEventListener("click", ingresar);
+});
+
 
 function ingresar(e){
     e.preventDefault();
+    validarRol()
 
-    window.location = "../../navbar.html";
+   
 }
+function recetearFormulario(e){
+    e.preventDefault();
+    formulario.reset();
+}
+
+
+function validarRol() {
+    //* Campos a validar
+
+    const email = document.querySelector('#emailUsuario');
+
+    const contrasena = document.querySelector('#contrasenaUsuario');
+
+    //- Expresiones Regulares
+    const number = /^\D*$/;
+    const text = /^[^a-zA-Z]*$/;
+    const email_val =
+        /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+
+    /// Lógica de validación
+
+    let isValidado = true;
+
+    //* Validaciones para el nombre
+
+
+    if (contrasena.value =="colchstar" && email.value=='tomasvanegas2533@gmail.com'){
+        isValidado=true
+    }else{
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Email o contraseña son incorrecta',
+             })
+         isValidado=false 
+
+    }
+
+
+   if (email.value == '') {
+
+        Swal.fire({
+            icon:'error',
+            title:'Error',
+            text: 'Ingrese el email'
+        })
+
+        isValidado = false;
+    }else if (!email_val.test(email.value) && email.value != '') {
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'El formato de gmail es incorrecto',
+             })
+        isValidado = false;
+    }else if (contrasena.value==''){
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Ingrese la contraseña',
+             })
+        isValidado = false;
+
+    }
+    
+   
+
+    if (isValidado) {
+        //* Serrando el modal
+        // const modalBootstrap = bootstrap.Modal.getInstance(
+        //     document.querySelector('#myModal')
+        // );
+        // modalBootstrap.hide();
+
+        formulario.reset();
+
+        window.location = "../../navbar.html";
+
+
+        
+         Swal.fire(
+            'Bienvenido a colch star '
+                 );
+
+    }
+}
+  
