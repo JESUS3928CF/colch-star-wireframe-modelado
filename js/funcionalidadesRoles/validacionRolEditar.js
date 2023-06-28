c3110b3f8af97d140701c21da1665679f0f4c36f
Ts2533 <tomasvanegas2533@gmail.com>
2023-06-27 19:50:50 -0500

    Modal de roles editar listo

--- js/funcionalidadesRoles/validacionRolEditar.js
+++ js/funcionalidadesRoles/validacionRolEditar.js
@@ -54,9 +54,12 @@
             Swal.fire({
                 icon:'error',
                 title: 'Error',
-                text: 'Todos los campos son obligatorios'
+                text: 'Todos los campos son obligatorios',
+                width: '700px'
+
             })
             isValidado=false
+
         }
         else if (nombre.value == '') {
 
@@ -65,6 +68,8 @@
                 icon: 'error',
                 title: 'Error',
                 text: 'El nombre es obligatorio',
+                width: '700px'
+
                  })
                    isValidado = false 
         } else if (!number.test(nombre.value)) {
@@ -72,6 +77,8 @@
                 icon: 'error',
                 title: 'Error',
                 text: 'El nombre no puede contener números',
+                width: '700px'
+
                  })
             isValidado = false; 
             
@@ -80,6 +87,8 @@
                     icon: 'error',
                     title: 'Error',
                     text: 'El nombre no puede ser un espacio',
+                    width: '700px'
+
                      })
                 isValidado = false;  
         }else if (signo.test(nombre.value)) {
@@ -87,20 +96,26 @@
                 icon: 'error',
                 title: 'Error',
                 text: 'No se puede poner signos en el nombre',
+                width: '700px'
+
                  })
             isValidado = false;
         }else if(elemento1==false &&elemento2 ==false && elemento3==false && elemento4 ==false){
             Swal.fire({
                 icon: 'error',
                 title: 'Error',
-                text: 'Seleccione por lo menos un permisos'
+                text: 'Seleccione por lo menos un permisos',
+                width: '700px'
+
             })
             isValidado=false
         }else if(select ==false && select1 ==false && select2 ==false && select3 ==false  && select4 ==false){
             Swal.fire({
                 icon:'error',
                 title: 'Error',
-                text: 'Seleccione por lo menos un  modulos'
+                text: 'Seleccione por lo menos un  modulos',
+                width: '700px'
+
             })
             isValidado=false
         }
